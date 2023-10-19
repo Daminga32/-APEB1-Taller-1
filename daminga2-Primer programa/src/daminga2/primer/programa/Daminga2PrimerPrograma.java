0a3b2a631d7dc7ebd8c599ed528d9619909cf754
Dario <daminga2@utpl.edu.ec>
2023-10-19 17:33:39 -0500

    nose

--- daminga2-Primer programa/src/daminga2/primer/programa/Daminga2PrimerPrograma.java
+++ daminga2-Primer programa/src/daminga2/primer/programa/Daminga2PrimerPrograma.java
@@ -1,5 +1,5 @@
 package daminga2.primer.programa;
 public class Daminga2PrimerPrograma {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo - profe");
     }}

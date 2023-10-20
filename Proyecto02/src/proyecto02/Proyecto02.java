be5dc67866fce75ff3efdfce4e019d044bfa4ffe
Dario <daminga2@utpl.edu.ec>
2023-10-20 13:24:06 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ Proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,10 @@
+package proyecto02;
+public class Proyecto02 {
+public static void main(String[] args) {
+    System.out.println("Ejemplo dos básico de Java\n" +
+"Versionado a través de Git\n" +
+"Manejado desde GitHub\n" +
+"Además se usar GitHubClassroom");
+    }
+    
+}

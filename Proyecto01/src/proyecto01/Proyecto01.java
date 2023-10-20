83bb1849e7d6d8e4bf1a54316113cc8a2deb4816
Dario <daminga2@utpl.edu.ec>
2023-10-20 13:13:28 -0500

    Primer commit

---
+++ Proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,9 @@
+
+package proyecto01;
+public class Proyecto01 {
+public static void main(String[] args) {
+    System.out.println("Ejemplo básico de Java" +"Versionado a través de Git\n" +"Manejado desde GitHub\n" +
+"Recuerde usar la sentencia");      
+    }
+    
+}
